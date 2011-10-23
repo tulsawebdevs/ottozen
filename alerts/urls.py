2d19b7c59d4b396405d05d52ed65831dae00b8ff
groovecoder <luke.crouch@gmail.com>
2011-10-22 20:26:14 -0500

    fix commute add view

--- alerts/urls.py
+++ alerts/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic.create_update import create_object
-from alerts.models import Commute
 
-urlpatterns = patterns('',
-    url('^commutes/add$', create_object, {'model': Commute, 'login_required': True}),
+urlpatterns = patterns('alerts.views',
+    url('^commutes/add$', 'add'),
 )
