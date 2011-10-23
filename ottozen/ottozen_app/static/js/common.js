b6cd9caa9d58f700f5a837889594db48dbbb4092
benjamin <benjamin@benjamin-5734z.(none)>
2011-10-23 15:57:03 -0500

    login form script skeleton

--- ottozen/ottozen_app/static/js/common.js
+++ ottozen/ottozen_app/static/js/common.js
@@ -3,8 +3,37 @@
 	$(document).ready(function(){
 		
 		console.log('herro ready');
-	
+		
+		loginForm();
+		
 	});
+	
+	function loginForm(){
+		if ( $('#login-cont').length ) {
+			
+			var form = $('#login-cont').find('form'),
+				uname = form.find('[name="dummy_name"]'),
+				pass = form.find('[name="password"]');
+			
+			form.submit(function(e){
+				
+				e.preventDefault();
+				
+				$.post( form.attr('action'), form.serialize(), function(data, status){
+					
+					console.log('login post', data, status);
+					
+					if ( status === 'success' ) {
+						//do stuff
+					} else {
+						//error, do other stuff
+					}
+				});
+				
+			});
+			
+		}
+	}
 	
 	
 })(jQuery)
