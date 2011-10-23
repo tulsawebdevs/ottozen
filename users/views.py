0c50e0a205daa2762fc41445caf5a37a269efb56
John Whitlock <John-Whitlock@ieee.org>
2011-10-22 21:18:15 -0500

    Add UserProfile if it doesn't exist

--- users/views.py
+++ users/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from alerts.models import Commute
 from users.forms import ProfileForm
-
+from users.models import UserProfile
 
 def profile(request):
   
+  try:
+    profile = request.user.get_profile()
+  except UserProfile.DoesNotExist:
+    profile = UserProfile.objects.create(user=request.user)
   if request.method == 'POST':
     data = {'user': request.user.id,
             'home_address': request.POST['home_address'],
             'home_city': request.POST['home_city'],
             'mobile_num': request.POST['mobile_num']}
-    form = ProfileForm(data, instance=request.user.get_profile())
+    form = ProfileForm(data, instance=profile)
     print request.POST
     print form.is_valid()
     print form.errors
@@ -18,7 +22,7 @@ def profile(request):
       print 'valid!'
       form.save()
   else:
-    form = ProfileForm(instance=request.user.get_profile())
+    form = ProfileForm(instance=profile)
 
   commutes = Commute.objects.filter(user=request.user)
   profile = request.user.get_profile()
