a75becc4ede461382ab9c5460203d5a475980784
groovecoder <luke.crouch@gmail.com>
2011-10-27 22:09:01 -0500

    draw trif incident pins

--- ottozen/ottozen_app/static/js/common.js
+++ ottozen/ottozen_app/static/js/common.js
@@ -103,6 +103,7 @@
 				zoom: me.zoom,
 				mapTypeId: google.maps.MapTypeId[ me.mapTypeId ]
 			});
+            me.loadTrifIncidents();
 			//add a dblclick listener to deal with way points
 			google.maps.event.addListener( me.map, 'dblclick', me.mapDoubleClick, false );
 			};
@@ -119,6 +120,76 @@
 				//console.log( 'current route & waypoint:', me.currentRoute, me.waypoints );
 			}
 		};
+        // From http://anentropic.wordpress.com/2009/06/25/javascript-iso8601-parser-and-pretty-dates/
+        me.parseISO8601 = function(str) {
+            // we assume str is a UTC date ending in 'Z'
+
+            var parts = str.split('T'),
+            dateParts = parts[0].split('-'),
+            timeParts = parts[1].split('Z'),
+            timeSubParts = timeParts[0].split(':'),
+            timeSecParts = timeSubParts[2].split('.'),
+            timeHours = Number(timeSubParts[0]),
+            _date = new Date;
+
+            _date.setUTCFullYear(Number(dateParts[0]));
+            _date.setUTCMonth(Number(dateParts[1])-1);
+            _date.setUTCDate(Number(dateParts[2]));
+            _date.setUTCHours(Number(timeHours));
+            _date.setUTCMinutes(Number(timeSubParts[1]));
+            _date.setUTCSeconds(Number(timeSecParts[0]));
+            if (timeSecParts[1]) _date.setUTCMilliseconds(Number(timeSecParts[1]));
+
+            // by using setUTC methods the date has already been converted to local time(?)
+            return _date;
+        }
+        me.drawTrifPins = function(data){
+            alerts = data.alerts;
+            var open_infowindow;
+            for(a in alerts){
+                var position = new google.maps.LatLng(alerts[a].geo.latitude, alerts[a].geo.longitude);
+                var icon;
+                if(alerts[a].type === 'closure'){
+                    icon = 'https://maps.google.com/mapfiles/ms/icons/red.png';
+                }else if(alerts[a].type === 'incident'){
+                    icon = 'https://maps.google.com/mapfiles/ms/icons/yellow.png';
+                }
+                var marker = new google.maps.Marker({
+                    position: position,
+                    map: me.map,
+                    title: alerts[a].title,
+                    icon: icon
+                });
+
+                d = me.parseISO8601(alerts[a].start);
+                start = d.toLocaleDateString() + " " +d.toLocaleTimeString();
+
+                info_content = '<dl>' +
+                    '<dt>Title</dt><dd>' + alerts[a].title + '</dd>' +
+                    '<dt>Description</dt><dd>' + alerts[a].description + '</dd>' +
+                    '<dt>Type</dt><dd>' + alerts[a].type + '</dd>' +
+                    '<dt>Category</dt><dd>' + alerts[a].category + '</dd>' +
+                    '<dt>Started</dt><dd>' + start + '</dd>' +
+                    '<dt>TRIF Details</dt><dd><a href="' + alerts[a].detail_link + '" target="trif_link">Trif</a></dd>';
+                if(alerts[a].hasOwnProperty('source_link')){
+                    info_content += '<dt>Source Details</dt><dd><a href="' + alerts[a].source_link + '" target="src_link">Source</a></dd>';
+                }
+                info_content += '</dl>';
+
+                marker.info_window = new google.maps.InfoWindow({
+                    content: info_content 
+                });
+
+                google.maps.event.addListener(marker, 'click', function(){
+                    if(open_infowindow){open_infowindow.close();}
+                    this.info_window.open(me.map, this);
+                    open_infowindow = this.info_window;
+                });
+            }
+        };
+        me.loadTrifIncidents = function(){
+            $.getJSON('http://trif.tulsawebdevs.org/alerts/incidents.jsonp?callback=?', me.drawTrifPins);
+        };
 		me.getRoute = function(){
 			//console.log( 'me.getRoute', me.startAddy, me.endAddy, me.waypoints );
 			me.directionsService.route({
